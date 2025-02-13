8c46b3fd0d4c2979fa94511002a759246ab846e8
SmartAzanClock <smartazanclock@gmail.com>
2025-02-12 20:26:10 -0500

    cancel up-to-date message

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# SmartAzanClock.com ReactJS PWA Application
+SmartAzanClock.com ReactJS PWA Application
 
-Use **SmartAzanClock.com** to turn any Android, Amazon Echo, or Raspberry Pi device into an actual adhan clock.
+Use SmartAzanClock.com to turn any Android, Amazon Echo, or Raspberry Pi device into an actual adhan clock for free!
 
-- **Free, safe, and open source**
-- **No registrations or subscriptions**
-- **Works even when offline**
+. Safe and open source
+. No registrations or subscriptions
+. Works even when offline
 
--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -6,7 +6,7 @@ const isLocalhost = Boolean(window.location.hostname === 'localhost');
 export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      const swUrl = '/service-worker.js'; 
+      const swUrl = '/service-worker.js';
       if (!isLocalhost) {
         navigator.serviceWorker.register(swUrl).then((registration) => {
           registration.onupdatefound = () => {
@@ -17,7 +17,7 @@ export function register() {
                   toast.info(
                     <div>
                       <a href="/" onClick={() => window.location.reload()}>A new version is available! Please click here to update.</a>
-                    </div>, 
+                    </div>,
                     { toastId: "PWAVersionUpdate", autoClose: false, closeOnClick: false, draggable: false }
                   );
                 }
@@ -28,9 +28,13 @@ export function register() {
           .catch((error) => {
             console.error('Error during service worker registration:', error);
           });
+
+        /*
         navigator.serviceWorker.addEventListener('controllerchange', () => {
           toast.success('You SmartAzanClock app is now up-to-date.', { toastId: "PWAUpdatedVersion", autoClose: 5000 });
         });
+        */
+       
       } else {
         console.log('Running on localhost. SW is not registered.');
       }
